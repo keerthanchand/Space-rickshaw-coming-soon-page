d9e1c451e68b76051593328e1d8d0336ac2f904b
Keerthan chand <keerthanchand@Keerthans-MacBook-Pro.local>
2024-05-20 02:54:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import moonImage from './moon.png'; // Make sure to add the moon image in the src folder
+import logo from './sr_logo.png'; // Make sure to add the moon image in the src folder
+import { Grid } from '@mui/material';
+import { TypeAnimation } from 'react-type-animation';
+import ReactGa from 'react-ga';
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-DKKCGH86KD"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-DKKCGH86KD');
+// </script>
+ReactGa.initialize("G-DKKCGH86KD")
 
 function App() {
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100%" }}>
+      <div>
+
+        <img src={logo} style={{ width: "270px", objectFit: "contain", marginTop: "15px", marginLeft: "10px" }} alt='space rickshaw logo'></img>
+      </div>
+      <Grid container spacing={5} alignItems="center" justifyContent="space-around" style={{ height: "90%" }}>
+
+        <Grid item md={6} xs={12}>
+          <img src={moonImage} alt='moon' style=
+            {{
+              maxWidth: "100%",
+              minWidth: "300px",
+              height: "auto"
+            }}></img>
+        </Grid>
+
+
+        <Grid item md={6} xs={12}>
+          <h1 style={{ color: "#fff", fontSize: "3rem", paddingLeft: "10px" }}>Coming Soon!</h1>
+          <div style={{ paddingRight: "25px", paddingLeft: "10px", height: "190px" }}>
+
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed once, initially
+                "Affordable and fast access to space with unmanned labs for experiments, manufacturing, research, art, and advertising in LEO.",
+                1000,
+                "Affordable and fast access to space with unmanned labs for experiments, manufacturing, research, art, and advertising in HEO.",
+                1000,
+                "Affordable and fast access to space with unmanned labs for experiments, manufacturing, research, art, and advertising in the Moon.",
+                1000,
+                "Affordable and fast access to space with unmanned labs for experiments, manufacturing, research, art, and advertising in Deep Space.",
+                1000,
+              ]}
+              speed={50}
+              style={{ fontSize: '1.4em', color: "#fff", }}
+              repeat={Infinity}
+            />
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
